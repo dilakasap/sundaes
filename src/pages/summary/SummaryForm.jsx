b2dac1af299526b1259a29e89b56cbc2bd1e5d00
Dila Kasap <dila.kasap@csadmins-MacBook-Pro.local>
2022-01-02 21:56:43 +0300

    added popover and its test

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, OverlayTrigger, Popover, PopoverBody } from "react-bootstrap";
 
 function SummaryForm() {
   const [isChecked, setIsChecked] = useState(false);
+
+  const popover = (
+    <Popover id="termsandconditions-popover">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to{" "}
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
   return (
     <Form>
-    <Form.Group controlId="terms-and-conditions">
-      <Form.Check
-        type="checkbox"
-        checked={isChecked}
-        onChange={(e) => {
-          setIsChecked(e.target.checked);
-        }}
-        label={checkboxLabel}
-      />
-    </Form.Group>
-    <Button variant="primary" type="submit" disabled={!isChecked}>
-      Confirm Order
-    </Button>
-  </Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={isChecked}
+          onChange={(e) => {
+            setIsChecked(e.target.checked);
+          }}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!isChecked}>
+        Confirm Order
+      </Button>
+    </Form>
   );
 }
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,21 +1,46 @@
-import { fireEvent, render,screen } from "@testing-library/react"
-import SummaryForm from "../SummaryForm"
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
-test("Is checkbox unchecked and button disabled", ()=>{
-    render(<SummaryForm/>);
-    const checkbox=screen.getByRole("checkbox", {name:"I agree to Terms and Conditions"});
-    const confirmButton=screen.getByRole("button", {name:"Confirm Order"});
-    expect(checkbox).not.toBeChecked();
-    expect(confirmButton).toBeDisabled();
+test("Is checkbox unchecked and button disabled", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(checkbox).not.toBeChecked();
+  expect(confirmButton).toBeDisabled();
 });
 
-test("Is checkbox checked and button enabled", ()=>{
-    render(<SummaryForm/>);
-    const checkbox=screen.getByRole("checkbox",{name:"I agree to Terms and Conditions"});
-    const confirmButton=screen.getByRole("button", {name:"Confirm Order"});
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeDisabled();
+test("Is checkbox checked and button enabled", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
+test("popover responde to hover",async () => {
+  render(<SummaryForm />);
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
 
-})
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(()=>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+  )
+});
